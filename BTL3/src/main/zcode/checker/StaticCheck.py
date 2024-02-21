240e715cace1c659f25480d70e5947a89606e884
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-02-21 14:34:48 +0700

    feat : Id check declared

--- BTL3/src/main/zcode/checker/StaticCheck.py
+++ BTL3/src/main/zcode/checker/StaticCheck.py
@@ -6,8 +6,9 @@ from functools import reduce
 
 
 class Symbol:
-    def __init__(self, name : str):
+    def __init__(self, name : str, type : Type = None):
         self.name = name
+        self.type = type
 
 def getName(symbol : Symbol):
     return symbol.name    
@@ -21,14 +22,14 @@ class VariableSymbol(Symbol):
         return f"VariableSymbol({self.name}, {self.type})"
 
 class FunctionSymbol(Symbol):
-    def __init__(self, name: str, returnType: Type = None, param: list[VariableSymbol] = [], body:Stmt =None):
+    def __init__(self, name: str, type: Type = None, param: list[VariableSymbol] = [], body:Stmt =None):
         self.name = name
-        self.returnType = returnType
+        self.type = type # return type of function
         self.param = param
         self.body = body
 
     def __str__(self):
-        return f"FunctionSymbol({self.name}, {self.returnType}, [{', '.join(str(i) for i in self.param)}], {self.body})"
+        return f"FunctionSymbol({self.name}, {self.type}, [{', '.join(str(i) for i in self.param)}], {self.body})"
 
 class Scope:
     def __init__(self, symbols: list[Symbol] = [Symbol]):
@@ -40,14 +41,23 @@ class Scope:
     def __str__(self) -> str:
         return f"Scope([{', '.join(str(i) for i in self.symbols)}])"
 
+class ExprParam: 
+    def __init__(self, kind : Kind, scopeIndex : int, isDeclared : bool = False) -> None:
+        self.kind = kind
+        self.scopeIndex = scopeIndex
+        self.isDeclared = isDeclared
+
+    def __str__(self) -> str:
+        return f"IdParam({self.kind}, {self.scopeIndex}, {self.isDeclared})"
+    
 
 class StaticChecker(BaseVisitor, Utils):
 
     def __init__(self, ast):
         self.ast = ast
-        self.envi = [Scope] # stack of Scope
+        self.envi : list[Scope] = [] # stack of Scope
 
-        global_scope = Scope([
+        global_scope = Scope([ # global scope built-in functions
             FunctionSymbol("readNumber", NumberType),
             FunctionSymbol("readString", StringType),
             FunctionSymbol("readBool", BoolType),
@@ -59,24 +69,38 @@ class StaticChecker(BaseVisitor, Utils):
 
         self.envi.append(global_scope) # global scope
 
-        print("StaticChecker: ", ast)
+        #print("StaticChecker: ", self.envi)
 
     def checkRedeclared(self, kind : Kind, name: str, lst : Scope):
-        print("checkRedeclared: ", kind, id, lst)
+        #print("checkRedeclared: ", kind, id, lst)
 
         symbols = lst.symbols
         if self.lookup(name, symbols, getName):
             raise Redeclared(kind, name)
         return False
     
+    def checkDeclared(self, kind : Kind, name : str, current_scope_index : int) -> Symbol:
+        #print("checkDeclared: ", id, lst)
+        
+        for i in range(current_scope_index, -1, -1): # from current scope to global scope
+            print("checkDeclared Scope: ", i, self.envi[i])
+            symbols = self.envi[i].symbols # list of Symbol in scope
+           
+            for symbol in symbols:
+                if name == getName(symbol):
+                    return symbol
+                
+        raise Undeclared(kind, name)
+    
+
     def checkEntry(self, name : str, lst : Scope):
-        print("checkEntry: ", id, lst)
+        #print("checkEntry: ", id, lst)
         symbols = lst.symbols
 
         if not self.lookup(id, symbols, getName):
             raise NoEntryPoint()
         return False
-
+        
 
     def check(self):
         return self.visit(self.ast, None)
@@ -90,7 +114,7 @@ class StaticChecker(BaseVisitor, Utils):
             self.visit(decl, None)
 
         # Check for entry point
-        self.checkEntry("main", self.envi[-1])
+        self.checkEntry("main", self.envi[0])
         
         self.envi.pop()
 
@@ -98,22 +122,32 @@ class StaticChecker(BaseVisitor, Utils):
         print(ast)
 
         name = self.visit(ast.name, None)
-        varInit = self.visit(ast.varInit, None) if ast.varInit else None
 
         if self.checkRedeclared(Variable(), name, self.envi[-1]):
             return
 
+        current_scope = self.envi[-1]
+        current_scope_index = len(self.envi) - 1
         
         # Visit variable type
         if ast.modifier == "var":
-            pass
-        elif ast.modifier == "dynamic":
-            pass
+            varInitType = self.visit(ast.varInit, None) if ast.varInit else None
+            current_scope.define(VariableSymbol(name, ast.varType))
+        
+        elif ast.modifier == "dynamic":    
+            varInitType = self.visit(ast.varInit, None) if ast.varInit else None
+            current_scope.define(VariableSymbol(name, ast.varType))
+        
         else:
-            pass
+            varType = self.visit(ast.varType, None)
+            if ast.varInit:
+                varInitType = self.visit(ast.varInit, ExprParam(Variable(), current_scope_index, True))
+                if type(varType) != type(varInitType):
+                    raise TypeMismatchInStatement(ast)
 
-        # Add variable to current scope
-        self.envi[-1].define(VariableSymbol(name, ast.varType))
+            self.envi[-1].define(VariableSymbol(name, varType))
+
+
     
     def visitFuncDecl(self, ast, param):
         print(ast)
@@ -149,19 +183,69 @@ class StaticChecker(BaseVisitor, Utils):
 
     
     def visitBinaryOp(self, ast, param):
-        pass
+        left = self.visit(ast.left, param)
+        right = self.visit(ast.right, param)
+
+        if ast.op in ['+', '-', '*', '/', '%']:
+            if isinstance(left, NumberType) and isinstance(right, NumberType):
+                return NumberType()
+            raise TypeMismatchInExpression(ast)
+        
+        if ast.op in ['>', '>=', '<', '<=', '=', '!=']:
+            if isinstance(left, NumberType) and isinstance(right, NumberType):
+                return BoolType()
+            raise TypeMismatchInExpression(ast)
+        
+        if ast.op in ['and', 'or']:
+            if isinstance(left, BoolType) and isinstance(right, BoolType):
+                return BoolType()
+            raise TypeMismatchInExpression(ast)
+        
+        if ast.op in ['...']:
+            if isinstance(left, StringType) and isinstance(right, StringType):
+                return StringType()
+            raise TypeMismatchInExpression(ast)
+        
+        if ast.op in ['==']:
+            if isinstance(left, StringType) and isinstance(right, StringType):
+                return BoolType()
+            raise TypeMismatchInExpression(ast)
+
+        return None # No type can be inferred
 
     
     def visitUnaryOp(self, ast, param):
-        pass
+        expr = self.visit(ast.operand, param)
+
+        if ast.op in ['-']:
+            if isinstance(expr, NumberType):
+                return NumberType()
+            raise TypeMismatchInExpression(ast)
+        
+        if ast.op in ['not']:
+            if isinstance(expr, BoolType):
+                return BoolType()
+            raise TypeMismatchInExpression(ast)
+
+        return None # No type can be inferred
 
     
     def visitCallExpr(self, ast, param):
         pass
 
     
+    
     def visitId(self, ast, param):
-        return ast.name
+        print("Visit Id: " , ast, param)
+        if isinstance(param, ExprParam):
+            if param.isDeclared:
+                symbol = self.checkDeclared(Identifier(), ast.name, param.scopeIndex)
+                
+                print("Visit Id Found: ", symbol)
+                return symbol.type
+        
+        else:
+            return ast.name
 
     
     def visitArrayCell(self, ast, param):
@@ -201,15 +285,15 @@ class StaticChecker(BaseVisitor, Utils):
 
     
     def visitNumberLiteral(self, ast, param):
-        pass
+        return NumberType()
 
     
     def visitBooleanLiteral(self, ast, param):
-        pass
+        return BoolType()
 
     
     def visitStringLiteral(self, ast, param):
-        pass
+        return StringType()
 
     
     def visitArrayLiteral(self, ast, param):
--- BTL3/src/test/CheckerSuite.py
+++ BTL3/src/test/CheckerSuite.py
@@ -8,14 +8,14 @@ class CheckerSuite(unittest.TestCase):
         input = """number a
         """
         expect = "No Entry Point"
-        self.assertTrue(TestChecker.test(input, expect, 400))
+        #self.assertTrue(TestChecker.test(input, expect, 400))
 
     def test_redeclared_variable(self):
         input = """number a
         number a
         """
         expect = "Redeclared Variable: a"
-        self.assertTrue(TestChecker.test(input, expect, 401))
+        #self.assertTrue(TestChecker.test(input, expect, 401))
 
     def test_redeclared_function(self):
         input = """
@@ -26,4 +26,78 @@ class CheckerSuite(unittest.TestCase):
             return 0
         """
         expect = "Redeclared Function: main"
-        self.assertTrue(TestChecker.test(input, expect, 402))
+        #self.assertTrue(TestChecker.test(input, expect, 402))
+
+    def test_redeclared_parameter(self):
+        input = """
+        func main(number a, number a) 
+            return 0
+        """
+        expect = "Redeclared Parameter: a"
+        #self.assertTrue(TestChecker.test(input, expect, 403))
+
+    def test_variable_declare(self):
+        input = """
+            string s1
+            bool b1
+            string s2 <- "1"
+            bool b2 <- true
+        
+        """
+        expect = "No Entry Point"
+        #self.assertTrue(TestChecker.test(input, expect, 404))
+
+    def test_variable_declare_2(self):
+        input = """
+            string s2 <- "1" ... "2"
+        """
+        expect = "No Entry Point"
+        #self.assertTrue(TestChecker.test(input, expect, 405))
+
+    def test_variable_declare_3(self):
+        input = """
+            string s2 <- "1" ... 2
+        """
+        expect = "Type Mismatch In Expression: BinaryOp(..., StringLit(1), NumLit(2.0)))"
+        #self.assertTrue(TestChecker.test(input, expect, 406))
+
+    def test_variable_declare_4(self):
+        input = """
+            number s2 <- "1" ... "2"
+        """
+        expect = "Type Mismatch In Statement: VarDecl(Id(s2), NumberType, None, BinaryOp(..., StringLit(1), StringLit(2))))"
+        #self.assertTrue(TestChecker.test(input, expect, 407))
+
+    def test_variable_declare_5(self):
+        input = """
+            bool s2 <- ("1" ... "2") == ("12" ... "2") 
+        """
+        expect = "No Entry Point"
+        #self.assertTrue(TestChecker.test(input, expect, 408))
+
+    def test_variable_declare_6(self):
+        input = """
+            bool b1 <- true
+            bool b2 <- true and b1
+            bool b3 <- (true and not b1) and b2
+        """
+        expect = "No Entry Point"
+        #self.assertTrue(TestChecker.test(input, expect, 409))
+
+    def test_variable_declare_7(self):
+        input = """
+            bool b1 <- (true and not b1)
+        """
+        expect = "Undeclared Identifier: b1"
+        #self.assertTrue(TestChecker.test(input, expect, 410))
+
+    def test_variable_declare_7(self):
+            input = """
+                bool b1 <- true
+            
+                func main() 
+                    bool b2 <- b1 and (1 <= 3)
+                    return 0
+            """
+            expect = "Undeclared Identifier: b1"
+            self.assertTrue(TestChecker.test(input, expect, 411))
